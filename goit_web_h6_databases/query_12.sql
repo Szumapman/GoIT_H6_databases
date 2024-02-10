053843744895adfd4a114acf1b88f68215121eae
Paweł Szumański <pawel.szumanski@tuta.io>
2024-02-10 13:34:43 +0100

    Create query_12.sql

---
+++ goit_web_h6_databases/query_12.sql
@@ -0,0 +1,8 @@
+-- Oceny uczniów w wybranej grupie z określonego przedmiotu na ostatnich zajęciach
+SELECT g.date_of AS data, c.course_name AS przedmiot, s.student_name, g.grade AS ocena
+FROM students s
+JOIN grades g ON s.id = g.student_id
+JOIN courses c ON g.course_id = c.id
+WHERE c.id  = 2
+  AND s.group_id = 3
+  AND g.date_of = (SELECT MAX(date_of) FROM grades);

e634d84df7cedac6814fd74365d9b06700433e74
Paweł Szumański <pawel.szumanski@tuta.io>
2024-02-10 13:36:40 +0100

    Update query_12.sql

--- goit_web_h6_databases/query_12.sql
+++ goit_web_h6_databases/query_12.sql
@@ -4,5 +4,5 @@ FROM students s
 JOIN grades g ON s.id = g.student_id
 JOIN courses c ON g.course_id = c.id
 WHERE c.id  = 2
-  AND s.group_id = 3
+  AND s.group_id = 2
   AND g.date_of = (SELECT MAX(date_of) FROM grades);

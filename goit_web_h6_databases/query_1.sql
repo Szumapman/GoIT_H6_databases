7a6b3fdf5e68dcedeb9ff081c921dc51194a6762
Paweł Szumański <pawel.szumanski@tuta.io>
2024-02-10 16:40:35 +0100

    Update query_1.sql

--- goit_web_h6_databases/query_1.sql
+++ goit_web_h6_databases/query_1.sql
@@ -1,8 +1,7 @@
 -- 5 uczniów z najwyższą średnią ocen ze wszystkich przedmiotów
-SELECT students.student_name, ROUND(AVG(grades.grade), 3) AS average_grade
-FROM students
-JOIN grades ON students.id = grades.student_id
-JOIN courses ON grades.course_id = courses.id
-GROUP BY students.id
-ORDER BY average_grade DESC
+SELECT s.id, s.student_name, ROUND(AVG(g.grade), 3) as avg_grades
+FROM students s
+JOIN grades g ON s.id = g.student_id
+GROUP BY s.id
+ORDER BY avg_grades DESC
 LIMIT 5;

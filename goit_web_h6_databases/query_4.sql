39edf75e1cd39300a2d19fa773c7d5d50ff8c584
Paweł Szumański <pawel.szumanski@tuta.io>
2024-02-12 15:42:55 +0100

    Update query_4.sql

--- goit_web_h6_databases/query_4.sql
+++ goit_web_h6_databases/query_4.sql
@@ -1,11 +1,8 @@
 -- Średnia ocen dla wszystkich grup, uwzględniając wszystkie oceny (dodaano kolejność od najwyższej średniej).
-SELECT g.group_name, ROUND(AVG(subquery.avg_grade), 3) AS srednia_ocen_grupy
-FROM groups g
-JOIN (
-    SELECT s.group_id, AVG(g.grade) AS avg_grade
-    FROM students s
-    JOIN grades g ON s.id = g.student_id
-    GROUP BY s.group_id
-) subquery ON g.id = subquery.group_id
-GROUP BY g.group_name
-ORDER BY srednia_ocen_grupy DESC;
+SELECT g2.group_name AS klasa, ROUND(AVG(g.grade), 3) AS srednia_ocen_klasy
+FROM grades g
+JOIN students s ON g.student_id = s.id
+JOIN courses c ON g.course_id = c.id
+JOIN groups g2 ON s.group_id = g2.id
+GROUP BY s.group_id
+ORDER BY srednia_ocen_klasy DESC;
